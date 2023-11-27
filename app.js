684035fba0527c01c8bfa5eecda2d86cec4dda16
ShivanG <shivangramola007@gmail.com>
2023-11-27 12:09:46 +0530

    updates

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const form = document.querySelector("form");
 
 function randomNumber() {
-  let num = Math.floor(Math.random() * 5) + 1;
+  let num = Math.floor(Math.random() * 10) + 1;
   return num;
 }
 
-const prevGuess = [];
+let prevGuess = [];
+
 let correct = 0;
 let remaining = 10;
 const guessRemain = document.querySelector(".guess-remain");
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
   const guess = document.querySelector(".guess");
   const error = document.querySelector(".error");
   const prev = document.querySelector(".prev-guess");
@@ -19,8 +21,8 @@ form.addEventListener("submit", function (event) {
 
   if (guess.value === "") {
     error.innerHTML = `<h3>box is empty</h3>`;
-  } else if (guessNum >= 1 && guessNum <= 100) {
-    if (remaining >= 1) {
+  } else if (guessNum >= 1 && guessNum <= 10) {
+    if (remaining > 1) {
       error.innerHTML = "";
       prevGuess.push(guessNum);
       prev.innerHTML = prevGuess;
@@ -35,15 +37,19 @@ form.addEventListener("submit", function (event) {
         error.innerHTML = `<h3>Guess is incorrect 😔😔😔</h3>`;
       }
     } else {
-      error.innerHTML = `<h3>game is over <br> ${correct} out of 10 are correct</h3>`;
+      if (remaining === 1) remaining = 0;
+      guessRemain.innerHTML = `${remaining}`;
+      error.innerHTML = `<h3>game is over 🎉🎉🎉 <br> ${correct} out of 10 are correct</h3>`;
     }
   } else {
-    if (remaining >= 1) {
-      error.innerHTML = `<h3>game is over</h3>`;
-    } else {
-      error.innerHTML = `<h3>number is out of range</h3>`;
-    }
+    error.innerHTML = `<h3>number is out of range</h3>`;
   }
 
   guess.value = "";
+});
+
+const again = document.querySelector(".again");
+
+again.addEventListener("click", function () {
+  location.reload();
 });
